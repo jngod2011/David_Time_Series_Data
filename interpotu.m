e1a561d60cb2e28430805e554788f723a277ad89
xiandalong <kelvinlong8897@gmail.com>
2015-08-11 12:26:22 -0400

    adding TS plots

---
+++ interpotu.m
@@ -0,0 +1,7 @@
+function Newotu=interpotu(day,otu)
+[M ~]=size(otu);
+L=length(day);
+iday=day(1):day(L);
+for i=1:M
+    Newotu(i,:) = interp1(day,otu(i,:),iday,'pchip');
+end
--- masterScript.m
+++ masterScript.m
@@ -1,4 +1,25 @@
-%% my version
+%% Preprocessing 
+%%%%%% their version %%%%%%%%%%%%%%%%%%%%%%%
+% isaA_otu=interpotu(saA_day,saA_otu(1:100,:));
+isaA_otu=interpotu(saA_day,saA_otu);
+isaA_notu=normotu(isaA_otu);
+% istA_otu=interpotu(stA_day,stA_otu(1:100,:));
+istA_otu=interpotu(stA_day,stA_otu);
+istA_notu=normotu(istA_otu);
+% istB_otu=interpotu(stB_day,stB_otu(1:100,:));
+istB_otu=interpotu(stB_day,stB_otu);
+istB_notu=normotu(istB_otu);
+[isaA_phy,saA_label]=phylumotu(isaA_notu,OTU_label);
+[istA_phy,stA_label]=phylumotu(istA_notu,OTU_label);
+[istB_phy,stB_label]=phylumotu(istB_notu,OTU_label);
+
+plot(isaA_phy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_saliva_A_stu','-dtiff');
+plot(istA_phy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_stool_A_stu','-dtiff');
+plot(istB_phy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_stool_B_stu','-dtiff');
+%%%%%% my version %%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1. combine the absolute abundance together
 [saA_phy,saA_label]=phylumotu(saA_otu,OTU_label);
 [stA_phy,stA_label]=phylumotu(stA_otu,OTU_label);
@@ -12,5 +33,13 @@ isaA_nphy=normotu(isaA_phy);
 istA_nphy=normotu(istA_phy);
 istB_nphy=normotu(istB_phy);
 
+plot(isaA_nphy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_saliva_A_mine','-dtiff');
+plot(istA_nphy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_stool_A_mine','-dtiff');
+plot(istB_nphy');
+print('Figures/raw TS plots/norm_interp_OTU_phy_TS_stool_B_mine','-dtiff');
+
 %% getting the periodiocity information
-% 1.
+% 1. FFT on the whole time series (interpolated)
+fft(
