72d5a1089888a966cc012eeeb1955dc9e7b89a4d
xiandalong <kelvinlong8897@gmail.com>
2015-08-19 11:30:13 -0400

    created .fig files for figures

--- plotFFTPSD.m
+++ plotFFTPSD.m
@@ -1,7 +1,7 @@
 function plotFFTPSD(ts, Fs)
 ts_norm = (ts-mean(ts))/std(ts);
 
-[pxx,f] = periodogram(ts_norm,[],2048,Fs); % same as using fft
+[pxx,f] = periodogram(ts_norm,[],512,Fs); % same as using fft
 
 plot(f,pxx);
 end
